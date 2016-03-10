f146261f94a67f0122ba2a2442843b33f8bcbe92
Pavel <nichitapavel@live.com>
2016-03-10 19:51:48 +0000

    Javadoc test

--- src/main/java/ull/project/App.java
+++ src/main/java/ull/project/App.java
@@ -8,7 +8,7 @@ public class App
 {
 	/**
 	 * Returns an Image object that can then be painted on the screen. 
-	 * The url argument must specify an absolute {@link URL}. The name
+	 * The url argument must specify an absolute. The name
 	 * argument is a specifier that is relative to the url argument. 
 	 * <p>
 	 * This method always returns immediately, whether or not the 
@@ -16,10 +16,8 @@ public class App
 	 * the screen, the data will be loaded. The graphics primitives 
 	 * that draw the image will incrementally paint on the screen. 
 	 *
-	 * @param  url  an absolute URL giving the base location of the image
-	 * @param  name the location of the image, relative to the url argument
+	 * @args  url  an absolute URL giving the base location of the image
 	 * @return      the image at the specified URL
-	 * @see         Image
 	 */
     public static void main( String[] args )
     {
