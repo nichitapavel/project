a6818b35cffc82a1a5955731d25b2faa76d82485
Pavel <nichitapavel@live.com>
2016-03-24 15:27:24 +0000

    Add methods isNull, hashCode, equals, toString
    Add methods isNull, hashCode, equals, toString and their respective
    Javadoc.

--- src/main/java/datastructures/Attribute.java
+++ src/main/java/datastructures/Attribute.java
@@ -16,10 +16,10 @@ public class Attribute implements Comparable<Attribute>{
     public Attribute() {
         // Manually generated default constructor.
     }
-    
-    
+        
     /**
      * Initializes the attribute string with the value of attribute.
+     * 
      * @param attribute A string representing the value of attribute.
      */
     public Attribute(String attribute) {
@@ -28,6 +28,7 @@ public class Attribute implements Comparable<Attribute>{
     
     /**
      * Initializes the attribute string with the value of obj.
+     * 
      * @param obj An Attribute object.
      */
     public Attribute(Attribute obj) {
@@ -36,6 +37,7 @@ public class Attribute implements Comparable<Attribute>{
     
     /**
      * Return the string with the value of Attribute.
+     * 
      * @return the string with the value of Attribute.
      */
     public String getAttribute() {
@@ -44,10 +46,77 @@ public class Attribute implements Comparable<Attribute>{
 
     /**
      * Sets the value of Attribute string.
+     * 
      * @param attribute the value to be set of attribute.
      */
     public void setAttribute(String attribute) {
         this.attr = attribute;
+    }
+    
+    /**
+     * Returns true if attribute is null o if length is 0, false otherwise.
+     * 
+     * @return true if attribute is null o if length is 0, false otherwise.
+     */
+    public boolean isNull() {
+        if ((attr == null) || (attr.length() == 0))
+            return true;
+        return false;
+    }
+    
+    /**
+     * Returns a hash code for this attribute.
+     * 
+     * The hash code for a Attribute object is
+     * computed using the default implementations of Eclipse.
+     * 
+     * @return a hash code value for this object.
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((attr == null) ? 0 : attr.hashCode());
+        return result;
+    }
+
+    /**
+     * Compares this attribute to the specified object.
+     * 
+     *  The result is true if and only if the argument
+     *  is not null and is a Attribute object that represents
+     *  the same attribute string as this object.
+     * 
+     * @param obj The object to compare this Attribute against.
+     * @return true if the given object represents a Attribute
+     * equivalent to this attribute, false otherwise.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Attribute other = (Attribute) obj;
+        if (attr == null || attr == "") {
+            if (other.attr != null && other.attr != "")
+                return false;
+        } else if (!attr.equals(other.attr))
+            return false;
+        return true;
+    }
+    
+    /**
+     * Returns the value of this attribute in string format.
+     * 
+     * @return the value of this attribute in string format.
+     */
+    @Override
+    public String toString() {
+        return attr;
     }
     
     /**
