c944e6613711a414f8ea1925467a149416a205ae
Pavel <nichitapavel@live.com>
2016-03-28 20:25:28 +0100

    Add method definitions to PluralDependency class

--- src/main/java/dependency/PluralDependency.java
+++ src/main/java/dependency/PluralDependency.java
@@ -28,4 +28,28 @@ public class PluralDependency extends ADependency {
         super(antecedent, consequent);
     }
 
+    @Override
+    public void clearTrivialElements() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
 }
