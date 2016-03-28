0bf2643ba9c038177074394ed72adbc42ac7fa79
Pavel <nichitapavel@live.com>
2016-03-28 19:38:22 +0100

    Add constant FD_NOT_INIT in Const class
    Add constant FD_NOT_INIT in Const class with value "FunctionalDependency
    not initialized".

--- src/main/java/utils/Const.java
+++ src/main/java/utils/Const.java
@@ -9,6 +9,7 @@ package utils;
  */
 public final class Const {
     public static final String ATTRJOINT_NOT_INIT = "AttributeJoint not initialized";
+    public static final String FD_NOT_INIT = "FunctionalDependency not initialized";
     
     private Const() {
         // Private constructor to prevent instantiation.
