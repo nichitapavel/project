0a0f9eeccba5c742af659aaeed80ce900ad1b8e5
Pavel <nichitapavel@live.com>
2016-03-30 00:03:21 +0100

    Add DFJoint class
    Add DFJoint class with these methods: DFJoint(), DFJoint(ArrayList),
    DFJoint(DFJoint), getDFJoint(), setDFJoint(ArrayList), getName(),
    setName(String), addDependency(ADependency), contains(ADependency),
    hashCode(), equals(Object) and iterator, all of them with their
    respective Javadoc and tests.

---
+++ src/main/java/datastructures/DFJoint.java
@@ -0,0 +1,172 @@
+/**
+ * 
+ */
+package datastructures;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import dependency.ADependency;
+
+/**
+ * @author Pavel Nichita
+ *
+ */
+public class DFJoint implements Iterable<ADependency> {
+    /**
+     * A list of functional dependencies defining this DFJoint
+     */
+    private List<ADependency> df;
+    /**
+     * A name to reference this DFJoint.
+     */
+    private String name;
+    
+    /**
+     * Constructs a Null DFJoint.
+     */
+    public DFJoint() {
+        df = new ArrayList<>();
+    }
+
+    /**
+     * Constructs a DFJoint with a list of functional dependencies and no name. 
+     * 
+     * @param dfJoint The list of dependencies.
+     */
+    public DFJoint(List<ADependency> dfJoint) {
+        this.df = new ArrayList<>(dfJoint);
+    }
+    
+    /**
+     * Constructs a DFJoint from another DFJoint.
+     * 
+     * @param dfJoint A DFJoint with a list of dependencies and name.
+     */
+    public DFJoint(DFJoint dfJoint) {
+        this.df = new ArrayList<>(dfJoint.getDFJoint());
+        this.name = dfJoint.getName();
+    }
+    
+    /**
+     * Returns the list of dependencies.
+     * 
+     * @return a list of dependencies.
+     */
+    public List<ADependency> getDFJoint() {
+        return df;
+    }
+
+    /**
+     * Sets the list of dependencies.
+     * 
+     * @param df The list of dependencies.
+     */
+    public void setDFJoint(ArrayList<ADependency> df) {
+        this.df = df;
+    }
+    
+    /**
+     * Sets the name of this DFJoint.
+     * 
+     * @param newName the name of this DFJoint.
+     */
+    public void setName(String newName) {
+        this.name = newName;
+    }
+    
+    /**
+     * Return the name of this DFJoint. 
+     * 
+     * @return the name of this DFJoint.
+     */
+    public String getName() {
+        if (this.name == null)
+            return "No name";
+        return this.name;
+    }
+    
+    /**
+     * Adds a new dependency to this objects list.
+     * 
+     * The dependency is added only if it is not already in the list.
+     * 
+     * @param df A dependency to be added to this objects list.
+     */
+    public void addDependency(ADependency df) {
+        if (!contains(df))
+            this.df.add(df);
+    }
+    
+    /**
+     * Returns if a dependency exist already in this objects list. 
+     * 
+     * Implied dependencies does not count.
+     *  
+     * @param funcDep The dependency to check if exists.
+     * @return true if {@code funcDep} exists in this list, false otherwise.
+     */
+    public boolean contains(ADependency funcDep) {
+        for (ADependency df : this.df)
+            if (df.equals(funcDep))
+                return true;
+        return false;
+    }
+    
+    /**
+     * Returns a hash code for this DFJoint.
+     * 
+     * The hash code for a DFJoint object is
+     * computed using the default implementations of Eclipse.
+     * 
+     * @return a hash code value for this object.
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((df == null) ? 0 : df.hashCode());
+        return result;
+    }
+
+    /**
+     * Compares this DFJoint to the specified object.
+     * 
+     *  The result is true if and only if the argument
+     *  is not null and is a DFJoint object that represents
+     *  the same attribute joint list as this object.
+     *  Only checks the list of dependencies, the name of this
+     *  DFJoint doesn't counts.
+     * 
+     * @param obj The object to compare this DFJoint against.
+     * @return true if the given object represents a DFJoint
+     * equivalent to this DFJoint, false otherwise.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DFJoint other = (DFJoint) obj;
+        if (df == null) {
+            if (other.df != null)
+                return false;
+        } else if (!df.equals(other.df))
+            return false;
+        return true;
+    }
+    
+    /**
+     * Returns an Iterator other the dependencies objects list.
+     * 
+     * @return an Iterator other the dependencies objects list.
+     */
+    @Override
+    public Iterator<ADependency> iterator() {
+        return this.df.iterator();
+    }
+}
---
+++ src/test/java/datastructures/DFJointTest.java
@@ -0,0 +1,375 @@
+/**
+ * 
+ */
+package datastructures;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+
+import dependency.ADependency;
+import dependency.FunctionalDependency;
+
+/**
+ * @author Pavel Nichita
+ *
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class DFJointTest {
+    private DFJoint nullDFJoint;
+    private ArrayList<ADependency> DFJointVector;
+    private ADependency one;
+    private ADependency two;
+    
+    // DFJoint = {A -> BC, BC -> A, BCD -> E, E -> C}
+    private DFJoint dfJoint;
+    //firstDFJoint  = {A -> BC, B -> C, A -> B, AB -> C}
+    private DFJoint firstDFJoint;
+    //secondDFJoint = {A -> B, B -> C}
+    private DFJoint secondDFJoint;
+    
+    private SetUpClass setUpObject;
+    /**
+     * Set Up.
+     */
+    @Before
+    public void setUp() {
+        this.setUpObject = new SetUpClass();
+        nullDFJoint = new DFJoint();
+        dfJoint = this.setUpObject.dfJoint01();
+        
+        this.one = this.setUpObject.funcDepAtoB();
+        this.two = this.setUpObject.funcDepCtoA();
+        
+        DFJointVector = new ArrayList<ADependency>();
+        DFJointVector.add(this.one);
+        DFJointVector.add(this.two);
+        
+        //firstDFJoint  = {A -> BC, B -> C, A -> B, AB -> C}
+        firstDFJoint = this.setUpObject.dfJoint04();
+        
+        //secondDFJoint = {A -> B, B -> C}
+        secondDFJoint = this.setUpObject.dfJoint05();
+    }
+
+    /**
+     * Test method for {@link datastructures.DFJoint#DFJoint()}.
+     * 
+     * Null constructor has size 0.
+     */
+    @Test
+    public void testDFJoint() {
+        assertEquals(0, nullDFJoint.getDFJoint().size());
+    }
+
+    /**
+     * Test method for {@link datastructures.DFJoint#DFJoint(java.util.List)}.
+     * 
+     * Checks every element after construct with a list of dependencies.
+     */
+    @Test
+    public void testConstructorArgumentVector() {
+        DFJoint auxDFJoint = new DFJoint(DFJointVector);
+        assertEquals(2, auxDFJoint.getDFJoint().size());
+        assertEquals(this.one, auxDFJoint.getDFJoint().get(0));
+        assertEquals(this.two, auxDFJoint.getDFJoint().get(1));
+    }
+    
+    /**
+     * Test method for {@link datastructures.DFJoint#DFJoint(DFJoint)}.
+     * 
+     * Checks the list and name after construct with another DFJoint.
+     */
+    @Test
+    public void testConstructorArgumentDFJoint() {
+        DFJoint auxDFJoint = new DFJoint(DFJointVector);
+        DFJoint testDFJoint = new DFJoint(auxDFJoint);
+        assertEquals(DFJointVector, testDFJoint.getDFJoint());
+        assertEquals(auxDFJoint.getName(), testDFJoint.getName());
+    }
+    
+    /**
+     * Test method for {@link datastructures.DFJoint#getDFJoint()}.
+     * 
+     * A DFJoint not null should not have it list size equal to zero.
+     */
+    @Test
+    public void testGetDFJointNotEqualZero() {
+        assertNotEquals(0, dfJoint.getDFJoint().size());
+    }
+    
+    /**
+     * Test method for {@link datastructures.DFJoint#getDFJoint()}.
+     * 
+     * Is equal list size to 4? Result True. 
+     */
+    @Test
+    public void testGetDFJointEqual() {
+        assertEquals(4, dfJoint.getDFJoint().size());
+    }
+    
+    /**
+     * Test method for {@link datastructures.DFJoint#getDFJoint()}.
+     * 
+     * A return list of non null DFJoint is no empty.
+     */
+    @Test
+    public void testGetDFJointNotNull() {
+        for (ADependency item : dfJoint.getDFJoint())
+            assertNotNull(item);
+    }
+    
+    /**
+     * Test method for {@link datastructures.DFJoint#getDFJoint()}.
+     * 
+     * The return list of dependencies are the ones expected.
+     */
+    @Test
+    public void testGetDFJoint() {
+        String [] antecedent = {"A"};
+        String [] consequent = {"B", "C"};
+        FunctionalDependency expected = new FunctionalDependency(
+                new AttributeJoint(antecedent), new AttributeJoint(consequent)
+                );
+        assertEquals(expected, dfJoint.getDFJoint().get(0));
+    }
+
+    /**
+     * Test method for {@link datastructures.DFJoint#setDFJoint(java.util.ArrayList)}.
+     * 
+     * Sets a list of dependencies, and checks that is returned the same list.
+     */
+    @Test
+    public void testSetDFJoint() {
+        nullDFJoint.setDFJoint(DFJointVector);
+        assertEquals(DFJointVector, nullDFJoint.getDFJoint());
+    }
+    
+    /**
+     * Test method for {@link datastructures.DFJoint#setDFJoint(java.util.ArrayList)}.
+     * 
+     * Sets a list of dependencies, and checks that is returned list has the right size.
+     */
+    @Test
+    public void testSetDFJointAssertSize() {
+        nullDFJoint.setDFJoint(DFJointVector);
+        assertEquals(2, nullDFJoint.getDFJoint().size());
+    }
+    
+    /**
+     * Test method for {@link datastructures.DFJoint#setDFJoint(java.util.ArrayList)}.
+     * 
+     * Sets a list of dependencies, and checks every dependency is in the returned list.
+     */
+    @Test
+    public void testSetDFJointAssertFunctionalDependencies() {
+        nullDFJoint.setDFJoint(DFJointVector);
+        assertEquals(this.one, nullDFJoint.getDFJoint().get(0));
+        assertEquals(this.two, nullDFJoint.getDFJoint().get(1));
+    }
+
+    /**
+     * Test method for {@link datastructures.DFJoint#setName(java.lang.String)}.
+     * 
+     * Sets name, checks that the returned name is correct.
+     */
+    @Test
+    public void testSetName() {
+        String expected = "DFJoint";
+        this.nullDFJoint.setName(expected);
+        assertEquals(expected, this.nullDFJoint.getName());
+    }
+
+    /**
+     * Test method for {@link datastructures.DFJoint#getName()}.
+     * 
+     * Checks that the returned names is the expected one.
+     */
+    @Test
+    public void testGetName() {
+        String expected = "No name";
+        assertEquals(expected, this.nullDFJoint.getName());
+    }
+
+    /**
+     * Test method for {@link datastructures.DFJoint#iterator()}.
+     * 
+     * Return iterator is not empty.
+     */
+    @Test
+    public void testIterator() {
+        for (ADependency item : dfJoint)
+            assertNotNull(item);
+    }
+
+    /**
+     * Test method for {@link datastructures.DFJoint#addDependency(ADependency)}.
+     * 
+     * Adds dependency to null DFJoint, checks that after size is 1 and
+     * that it got inserted.
+     */
+    @Test
+    public void testAddFuncDepToEmptyDFJoint() {
+        nullDFJoint.addDependency(this.one);
+        assertEquals(1, nullDFJoint.getDFJoint().size());
+        assertEquals(this.one, nullDFJoint.getDFJoint().get(0));
+    }
+    
+    /**
+     * Test method for {@link datastructures.DFJoint#addDependency(ADependency)}.
+     * 
+     * Adds dependency to a DFJoint, checks that after size is +1 and
+     * that it got inserted.
+     */
+    @Test
+    public void testAddFuncDepToNormalDFJoint() {
+        dfJoint.addDependency(this.one);
+        assertEquals(5, dfJoint.getDFJoint().size());
+        assertEquals(this.one, dfJoint.getDFJoint().get(4));
+    }
+    
+    /**
+     * Test method for {@link datastructures.DFJoint#addDependency(ADependency)}.
+     * 
+     * Adds dependency that already exists to DFJoint, checks that after size didn't
+     * changed and that has the same list of dependencies.
+     */
+    @Test
+    public void testAddFuncDepThatExist() {
+        DFJoint expected = this.setUpObject.dfJoint04();
+        this.firstDFJoint.addDependency(this.setUpObject.funcDepAtoBC());
+        assertEquals(expected, this.firstDFJoint);
+    }
+
+    /**
+     * Test method for {@link datastructures.DFJoint#hashCode()}.
+     * 
+     * Checks that two DFJoint with same values have same hashCode.
+     */
+    @Test
+    public void testHashCodeDFJointsWithSameValues() {
+        DFJoint auxDFJoint = new SetUpClass().dfJoint01();
+        assertEquals(dfJoint.hashCode(), auxDFJoint.hashCode());
+    }
+    
+    /**
+     * Test method for {@link datastructures.DFJoint#hashCode()}.
+     * 
+     * Checks that two different DFJoint have different hashCode.
+     */
+    @Test
+    public void testHashCodeDifferentDFJoints() {
+        assertNotEquals(dfJoint.hashCode(), nullDFJoint.getDFJoint());
+    }
+    
+    /**
+     * Test method for {@link datastructures.DFJoint#hashCode()}.
+     * 
+     * Checks hashCode value of an empty DFJoint. 
+     */
+    @Test
+    public void testHashCodeEmptyValueDFJoint() {
+        nullDFJoint.setDFJoint(null);
+        assertEquals(31, nullDFJoint.hashCode());
+    }
+    
+    /**
+     * Test method for {@link datastructures.DFJoint#equals(Object)}.
+     * 
+     * Same DFJoint is equal to himself.
+     */
+    @Test
+    public void testEqualsTrueSameObject() {
+        assertTrue(dfJoint.equals(dfJoint));
+    }
+    
+    /**
+     * Test method for {@link datastructures.DFJoint#equals(Object)}.
+     * 
+     * DFJoint is not equal to null.
+     */
+    @Test
+    public void testEqualsFalseNullObject() {
+        assertFalse(dfJoint.equals(null));
+    }
+    
+    /**
+     * Test method for {@link datastructures.DFJoint#equals(Object)}.
+     * 
+     * DFJoint is not equal to different class.
+     */
+    @Test
+    public void testEqualsFalseDifferentClassObject() {
+        Attribute A = new Attribute("A");
+        assertFalse(dfJoint.equals(A));
+    }
+    
+    /**
+     * Test method for {@link datastructures.DFJoint#equals(Object)}.
+     * 
+     * One good DFJoint is not equal to one with no list of dependencies.
+     */
+    @Test
+    public void testEqualsFalseDFJointWithNullValue() {
+        nullDFJoint.setDFJoint(null);
+        assertFalse(nullDFJoint.equals(dfJoint));
+    }
+    
+    /**
+     * Test method for {@link datastructures.DFJoint#equals(Object)}.
+     * 
+     * Two different DFJoints are not equal.
+     */
+    @Test
+    public void testEqualsFalseDFJointWithDifferentValues() {
+        nullDFJoint.addDependency(this.one);
+        assertFalse(dfJoint.equals(nullDFJoint));
+    }
+    
+    /**
+     * Test method for {@link datastructures.DFJoint#equals(Object)}.
+     * 
+     * Two DFJoints with same value are equal.
+     */
+    @Test
+    public void testEqualsTrueDFJoints() {
+        DFJoint auxDFJoint = new SetUpClass().dfJoint01();
+        assertTrue(dfJoint.equals(auxDFJoint));
+    }
+    
+    /**
+     * Test method for {@link datastructures.DFJoint#equals(Object)}.
+     * 
+     * Two DFJoints with same null lists are equal.
+     */
+    @Test
+    public void testEqualsTrueNullDFJoint() {
+        nullDFJoint.setDFJoint(null);
+        dfJoint.setDFJoint(null);
+        assertTrue(dfJoint.equals(nullDFJoint));
+    }
+    
+    /**
+     * Test method for {@link datastructures.DFJoint#contains(ADependency)}.
+     * 
+     * DFJoint contains A to BC? Result true.
+     */
+    @Test
+    public void containsTrue() {
+        assertTrue(this.firstDFJoint.contains(this.setUpObject.funcDepAtoBC()));
+    }
+    
+    /**
+     * Test method for {@link datastructures.DFJoint#contains(ADependency)}.
+     * 
+     * DFJoint contains E to C? Result false.
+     */
+    public void containsFalse() {
+        assertFalse(this.firstDFJoint.contains(this.setUpObject.funcDepEtoC()));
+    }
+}
