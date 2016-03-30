3fc114a98a5b2a3f4835260c3b1a2ae711659425
Pavel <nichitapavel@live.com>
2016-03-30 21:21:00 +0100

    Add RareElement class.
    Add RareElement class with these methods: RareElement(),
    RareELement(Attribute, ADependency. DFJoint, String), getAttribute(),
    getFD(), getDFJoint(), getPosition(), toString(), hashCode() and
    equals(Object). Constructors and toString() have tests, all methods have
    Javadoc.

---
+++ src/main/java/datastructures/RareElement.java
@@ -0,0 +1,149 @@
+/**
+ * 
+ */
+package datastructures;
+
+import dependency.ADependency;
+
+/**
+ * @author Pavel Nichita
+ *
+ */
+public class RareElement {
+    private Attribute attr;
+    private ADependency fd;
+    private DFJoint dfJoint;
+    /**
+     * Position in dependency, only has two possible values: antecedent and consequent.
+     */
+    private String position;
+    
+    /**
+     * Constructs a Null RareElement object.
+     */
+    public RareElement() {
+        // Manually generated default constructor.
+    }
+
+    /**
+     * Constructs a RareElement object with all the necessary information.
+     * 
+     * @param attr The rare attribute.
+     * @param fd The dependency where is rare.
+     * @param dfJoint The DFJoint where is rare.
+     * @param position If is in "antecedent" or "consequent".
+     */
+    public RareElement(Attribute attr, ADependency fd, DFJoint dfJoint, String position) {
+        this.attr = new Attribute(attr);
+        this.fd = fd;
+        this.dfJoint = new DFJoint(dfJoint);
+        this.position = position;
+    }
+    
+    /**
+     * Returns an attribute object that is rare.
+     * 
+     * @return an attribute object.
+     */
+    public Attribute getAttribute() {
+        return this.attr;
+    }
+
+    /**
+     * Returns the dependency where {@code attr} is rare.
+     * 
+     * @return the dependency where {@code attr} is rare.
+     */
+    public ADependency getFD() {
+        return this.fd;
+    }
+
+    /**
+     * Returns the DFJoint where {@code attr} is rare.
+     * 
+     * @return the DFJoint where {@code attr} is rare.
+     */
+    public DFJoint getDFJoint() {
+        return this.dfJoint;
+    };
+    
+    /**
+     * Returns where {@code attr} is rare: antecedent or consequent.
+     * 
+     * @return "consequent" or "antecedent".
+     */
+    public String getPosition() {
+        return this.position;
+    }
+    
+    /**
+     * Returns the value of this RareElement object in string format.
+     * 
+     * The format being used is <i>//** obj, position, dependency, dfjoint-name}</i>.
+     * If attribute is null, it returns a "Null" string.
+     * 
+     * @return a string representing this object, if the list is empty or
+     * null returns "Null" string. 
+     */
+    @Override
+    public String toString() {
+        if (this.attr != null)
+            return "//** " + this.attr.toString() + ", " + this.position + ", "
+                + this.fd.toString() + ", " + this.dfJoint.getName();
+        return "Null";
+    }
+    
+    /**
+     * Returns a hash code for this RareElement.
+     * 
+     * The hash code for a RareElement object is
+     * computed using the default implementations of Eclipse.
+     * 
+     * @return a hash code value for this object.
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((attr == null) ? 0 : attr.hashCode());
+        result = prime * result + ((dfJoint == null) ? 0 : dfJoint.hashCode());
+        result = prime * result + ((fd == null) ? 0 : fd.hashCode());
+        result = prime * result + ((position == null) ? 0 : position.hashCode());
+        return result;
+    }
+
+    /**
+     * Compares this RareElement to the specified object.
+     * 
+     * @param obj The object to compare this RareElement against.
+     * @return true if the given object represents a RareElement
+     * equivalent to this RareElement, false otherwise.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        RareElement other = (RareElement) obj;
+        if (attr == null) {
+            if (other.attr != null)
+                return false;
+        } else if (!attr.equals(other.attr))
+            return false;
+        if (dfJoint == null) {
+            if (other.dfJoint != null)
+                return false;
+        } else if (!dfJoint.equals(other.dfJoint))
+            return false;
+        if (fd == null) {
+            if (other.fd != null)
+                return false;
+        } else if (!fd.equals(other.fd))
+            return false;
+        return true;
+    }
+
+}
---
+++ src/test/java/datastructures/RareElementTest.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package datastructures;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+
+import dependency.ADependency;
+
+/**
+ * @author Pavel Nichita
+ *
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class RareElementTest {
+    private RareElement rareElement;
+    private RareElement rareElementVoid;
+    private SetUpClass setUpObject;
+    
+    /**
+     * 
+     */
+    @Before
+    public void setUp() {
+        this.setUpObject = new SetUpClass();
+        Attribute attr = this.setUpObject.attrA();
+        ADependency fd = this.setUpObject.funcDepAtoB();
+        DFJoint dfJoint = this.setUpObject.dfJoint04();
+        this.rareElement = new RareElement(attr, fd, dfJoint, "Antecedent");
+        this.rareElementVoid = new RareElement();
+    }
+
+    /**
+     * Test method for {@link datastructures.RareElement#RareElement()}.
+     * Checks that a null RareElement has all of his attributes to null.
+     */
+    @Test
+    public void testRareElement() {
+        assertNull(this.rareElementVoid.getAttribute());
+        assertNull(this.rareElementVoid.getFD());
+        assertNull(this.rareElementVoid.getDFJoint());
+        assertNull(this.rareElementVoid.getPosition());
+    }
+
+    /**
+     * Test method for {@link datastructures.RareElement#RareElement(datastructures.Attribute, dependency.ADependency, datastructures.DFJoint, java.lang.String)}.
+     * Checks that a RareElement has all of his attributes with content.
+     */
+    @Test
+    public void testRareElementAttributeADependencyDFJointString() {
+        assertEquals(this.setUpObject.attrA(), this.rareElement.getAttribute());
+        assertEquals(this.setUpObject.funcDepAtoB(), this.rareElement.getFD());
+        assertEquals(this.setUpObject.dfJoint04(), this.rareElement.getDFJoint());
+        assertEquals("Antecedent", this.rareElement.getPosition());
+    }
+
+    /**
+     * Test method for {@link datastructures.RareElement#toString()}.
+     * Checks if the string returned is good.
+     */
+    @Test
+    public void testToString() {
+        String expected = "//** A, Antecedent, {A} -> {B}, DFJoint Four";
+        assertEquals(expected, this.rareElement.toString());
+    }
+    
+    /**
+     * Test method for {@link datastructures.RareElement#toString()}.
+     * Checks if a null RareElement returns "Null" string. 
+     */
+    @Test
+    public void testToStringNull() {
+        String expected = "Null";
+        assertEquals(expected, this.rareElementVoid.toString());
+    }
+
+}
