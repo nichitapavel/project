76f66d88dc9fafe612c14a7bb72615065d8363af
Pavel <nichitapavel@live.com>
2016-03-31 23:34:16 +0100

    Add methods definition to PluralDependency
    Add methods definition to PluralDependency: hashCode(), equals(Object)
    and toFunctionalDependency(DFJoint) with their respective Javadoc, no
    tests.

--- src/main/java/dependency/PluralDependency.java
+++ src/main/java/dependency/PluralDependency.java
@@ -44,22 +44,81 @@ public class PluralDependency extends ADependency {
         return "";
     }
 
+    /**
+    * Returns a hash code for this plural dependency.
+    * 
+    * The hash code for a PluraDependency object is
+    * computed using the default implementations of Eclipse.
+    * 
+    * @return a hash code value for this object.
+    */
     @Override
     public int hashCode() {
-        // TODO Auto-generated method stub
-        return 0;
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((super.antecedent == null) ? 0 : super.antecedent.hashCode());
+        result = prime * result
+                + ((super.consequent == null) ? 0 : super.consequent.hashCode());
+        return result;
     }
 
+    /**
+     * Compares this plural dependency to the specified object.
+     * 
+     *  The result is true if and only if the argument
+     *  is not null and is a PluralDependency object that represents
+     *  the same attribute joint list as this object.
+     * 
+     * @param obj The object to compare this PluralDependency against.
+     * @return true if the given object represents a PluralDependency
+     * equivalent to this plural dependency, false otherwise.
+     */
     @Override
     public boolean equals(Object obj) {
-        // TODO Auto-generated method stub
-        return false;
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ADependency other = (ADependency) obj;
+        if (super.antecedent == null) {
+            if (other.antecedent != null)
+                return false;
+        } else if (!super.antecedent.equals(other.antecedent))
+            return false;
+        if (super.consequent == null) {
+            if (other.consequent != null)
+                return false;
+        } else if (!super.consequent.equals(other.consequent))
+            return false;
+        return true;
     }
 
+    /**
+     * Returns a list with functional dependencies that can be implied from {@code dfJoint}.
+     * 
+     * If a the antecedent of a functional dependency from {@code dfJoint} and consequent
+     * from this object have none common attributes but the consequent of the functional dependency
+     * is part of this consequent then there is a functional dependency with this antecedent that defines
+     * functional dependency consequent.
+     * 
+     * @param dfJoint the DFJoint where this dependency exists.
+     * @return a list of size one with this dependency in it.
+     */
     @Override
     public List<ADependency> toFunctionalDependency(DFJoint dfJoint) {
-        // TODO Auto-generated method stub
-        return new ArrayList<>();
+        List<ADependency> result = new ArrayList<>();      
+        AttributeJoint intersect;
+        for (ADependency fd : dfJoint) {
+            if (fd.getClass() == new FunctionalDependency().getClass()) {
+                intersect = fd.getAntecedent().intersect(super.consequent);
+                if ( fd.getConsequent().isContained(super.consequent) && intersect.isNull())
+                    result.add(new FunctionalDependency(super.antecedent, fd.getConsequent()));
+            }
+        }
+        return result;
     }
 
 }
