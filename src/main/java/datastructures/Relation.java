47e01fa74938735484b360ddcc54e41a7db11c7e
Pavel <nichitapavel@live.com>
2016-04-03 20:27:50 +0100

    Add methods to Relation class
    Add methods to Relation class: Relation(), Relation(AttributeJoint,
    DFJoint), getAttrJoint(), getName(), getDFJoint(),
    settAttrJoint(AttributeJoint), setDFJoint(DFJoint), setName(String),
    toString(), hashCode() and equals(Object) all of them with their
    respective Javadoc, most of them with tests.

--- src/main/java/datastructures/Relation.java
+++ src/main/java/datastructures/Relation.java
@@ -8,5 +8,164 @@ package datastructures;
  *
  */
 public class Relation {
+    private String name;
+    private AttributeJoint attrJoint;
+    private DFJoint dfJoint;
+    
+    /**
+     * Constructs a Null Relation.
+     */
+    public Relation() {
+        // Manually generated default constructor.
+    }
+
+    /**
+     * Constructs a Relation with a list of attributes and a DFJoint,
+     * but without a name.
+     * 
+     * @param attrJoint The list of attributes.
+     * @param dfJoint The DFJoint of this Relation. 
+     */
+    public Relation(AttributeJoint attrJoint, DFJoint dfJoint) {
+        this.attrJoint = new AttributeJoint(attrJoint);
+        this.dfJoint = new DFJoint(dfJoint);
+    }
+    
+    /**
+     * Returns the list of attributes of this Relation.
+     * 
+     * @return the list of attributes of this Relation.
+     */
+    public AttributeJoint getAttrJoint() {
+        return this.attrJoint;
+    }
+
+    /**
+     * Returns the name of this Relation.
+     * 
+     * @return the name of this Relation.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the DFJoint of this Relation.
+     * 
+     * @return the DFJoint of this Relation. 
+     */
+    public DFJoint getDFJoint() {
+        return this.dfJoint;
+    }
+    
+    /**
+     * Sets the attribute list of this Relation.
+     * 
+     * @param attrJoint The attribute list of this Relation.
+     */
+    public void settAttrJoint(AttributeJoint attrJoint) {
+        this.attrJoint = new AttributeJoint(attrJoint);
+    }
+
+    /**
+     * Sets the DFJoint of this Relation.
+     * 
+     * @param dfJoint The DFJoint of this Relation.
+     */
+    public void setDFJoint(DFJoint dfJoint) {
+        this.dfJoint = new DFJoint(dfJoint);
+    }
+    
+    /**
+     * Sets the name of this Relation.
+     * 
+     * @param name The name of this Relation.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    /**
+     * Returns a string representation of this Relation.
+     * 
+     * If this Relation has no attribute list or DFJoint returns an 
+     * "Sin Attributos Sin Dependencias Funcionales" message.
+     * 
+     * @return a string representation of this Relation.
+     */
+    @Override
+    public String toString() {
+        String noAttr = "Sin Atributos";
+        String noFuncDep = "Sin Dependencias Funcionales";
+        String msg = "";
+        
+        if (this.attrJoint != null)
+            msg += "Atributos: " + this.attrJoint.toString() + "";
+        else
+            msg += noAttr;
+        
+        if (this.dfJoint != null)
+            msg += " Dependencias Funcionales: " + this.dfJoint.toString();
+        else
+            msg += " " + noFuncDep;
+        
+        if (this.attrJoint == null && this.dfJoint == null)
+            return noAttr + " " + noFuncDep;
+        
+        return msg;
+    }
+
+    /**
+     * Returns a hash code for this Relation.
+     * 
+     * The hash code for a Relation object is computed using
+     *  the default implementations of Eclipse. The name of this Relation
+     *  doesn't influence the final result.
+     * 
+     * @return a hash code value for this object.
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((attrJoint == null) ? 0 : attrJoint.hashCode());
+        result = prime * result + ((dfJoint == null) ? 0 : dfJoint.hashCode());
+        return result;
+    }
+
+    /**
+     * Compares this DFJoint to the specified object.
+     * 
+     *  The result is true if and only if the argument
+     *  is not null and is a Relation object that represents
+     *  the same Relation as this object.
+     *  Only checks the list of dependencies, the name of this
+     *  Relation doesn't counts.
+     * 
+     * @param obj The object to compare this Relation against.
+     * @return true if the given object represents a Relation
+     * equivalent to this Relation, false otherwise.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Relation other = (Relation) obj;
+        if (attrJoint == null) {
+            if (other.attrJoint != null)
+                return false;
+        } else if (!attrJoint.equals(other.attrJoint))
+            return false;
+        if (dfJoint == null) {
+            if (other.dfJoint != null)
+                return false;
+        } else if (!dfJoint.equals(other.dfJoint))
+            return false;
+        return true;
+    }
 
 }
---
+++ src/test/java/datastructures/RelationTests.java
@@ -0,0 +1,191 @@
+/**
+ * 
+ */
+package datastructures;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Pavel Nichita
+ *
+ */
+public class RelationTests {
+    private Relation nullRelation;
+    //AB -> D, B -> C, C -> B, B -> E
+    private Relation firstFormRelation;
+    //A -> B, B -> C, C -> D
+    private Relation secondFormRelation;
+    //B -> C, AB -> D, C -> B
+    private Relation thirdFormRelation;
+    //AB -> C
+    private Relation boyceCoddFormRelation;
+    //A -> BC, BC -> A, BCD -> E, E -> C
+    private Relation one;
+    
+    //DFJoints
+    private DFJoint secondFormRelationDFJoint;
+    private DFJoint thirdFormRelationDFJoint;
+    private DFJoint boyceCoddFormRelationDFJoint;
+    
+    //AttributesJoints of relations attributes
+    private AttributeJoint secondAndThirdFormRelationAttributes;
+    private AttributeJoint boyceCoddFormRelationAttributes;
+    
+    //KeyJoint
+    private KeyJoint secondFormRelationKeyJoint;
+    private KeyJoint thirdFormRelationKeyJoint;
+    private KeyJoint boyceCoddFormRelationKeyJoint;
+    
+    private SetUpClass setUpObject;
+
+    /**
+     * Set up.
+     */
+    @Before
+    public void setUp() {
+        this.setUpObject = new SetUpClass();
+        
+        this.nullRelation = new Relation();
+        
+        SetUpClass setUpObject = new SetUpClass();
+        
+        //DFJoints
+        this.secondFormRelationDFJoint = setUpObject.dfJoint07();
+                
+        this.thirdFormRelationDFJoint = setUpObject.dfJoint08();
+        
+        this.boyceCoddFormRelationDFJoint = setUpObject.dfJoint09();
+        
+        //AttributeJoints attributes of a relation
+        this.secondAndThirdFormRelationAttributes = setUpObject.attrJntABCD();
+                
+        this.boyceCoddFormRelationAttributes = setUpObject.attrJntABC();
+
+        //Relations
+        this.firstFormRelation = new Relation(setUpObject.attrJntABCDE(), setUpObject.dfJoint06());
+        this.secondFormRelation = new Relation(secondAndThirdFormRelationAttributes, secondFormRelationDFJoint);
+        this.thirdFormRelation = new Relation(secondAndThirdFormRelationAttributes, thirdFormRelationDFJoint);
+        this.boyceCoddFormRelation = new Relation(boyceCoddFormRelationAttributes, boyceCoddFormRelationDFJoint);
+        this.one = this.setUpObject.relation01();
+        
+        //KeyJoint
+        List<AttributeJoint> keysVector = new ArrayList<>();
+        keysVector.add(setUpObject.attrJntAB());
+        
+        this.boyceCoddFormRelationKeyJoint = new KeyJoint(keysVector);
+        
+        keysVector.add(setUpObject.attrJntAC());
+        this.thirdFormRelationKeyJoint = new KeyJoint(keysVector);
+                
+        keysVector.clear();
+        keysVector.add(setUpObject.attrJntA());
+        
+        this.secondFormRelationKeyJoint = new KeyJoint(keysVector);
+    }
+
+    /**
+     * Test method for {@link datastructures.Relation#Relation()}.
+     */
+    @Test
+    public void testRelation() {
+        assertNotNull(this.nullRelation);
+    }
+
+    /**
+     * Test method for {@link datastructures.Relation#Relation(datastructures.AttributeJoint, datastructures.DFJoint)}.
+     */
+    @Test
+    public void testRelationAttributeJointDFJoint() {
+        assertEquals(secondAndThirdFormRelationAttributes, secondFormRelation.getAttrJoint());
+        assertEquals(secondFormRelationDFJoint, secondFormRelation.getDFJoint());
+    }
+
+    /**
+     * Test method for {@link datastructures.Relation#getAttrJoint()}.
+     */
+    @Test
+    public void testGetAttrJoint() {
+        assertEquals(secondAndThirdFormRelationAttributes, secondFormRelation.getAttrJoint());
+        assertEquals(secondAndThirdFormRelationAttributes, thirdFormRelation.getAttrJoint());
+        assertEquals(boyceCoddFormRelationAttributes, boyceCoddFormRelation.getAttrJoint());
+        assertNull(nullRelation.getAttrJoint());
+    }
+
+    /**
+     * Test method for {@link datastructures.Relation#getDFJoint()}.
+     */
+    @Test
+    public void testGetDFJoint() {
+        assertEquals(secondFormRelationDFJoint, secondFormRelation.getDFJoint());
+        assertEquals(thirdFormRelationDFJoint, thirdFormRelation.getDFJoint());
+        assertEquals(boyceCoddFormRelationDFJoint, boyceCoddFormRelation.getDFJoint());
+        assertNull(nullRelation.getDFJoint());
+    }
+
+    /**
+     * Test method for {@link datastructures.Relation#settAttrJoint(datastructures.AttributeJoint)}.
+     */
+    @Test
+    public void testSettAttrJoint() {
+        this.nullRelation.settAttrJoint(boyceCoddFormRelationAttributes);
+        assertEquals(boyceCoddFormRelationAttributes, this.nullRelation.getAttrJoint());
+    }
+
+    /**
+     * Test method for {@link datastructures.Relation#setDFJoint(datastructures.DFJoint)}.
+     */
+    @Test
+    public void testSetDFJoint() {
+        this.nullRelation.setDFJoint(boyceCoddFormRelationDFJoint);
+        assertEquals(boyceCoddFormRelationDFJoint, this.nullRelation.getDFJoint());
+    }
+
+    /**
+     * Test method for {@link datastructures.Relation#toString()}.
+     */
+    @Test
+    public void testToString() {
+        String msg2FN = "Atributos: {A, B, C, D} Dependencias Funcionales: { ({A} -> {B}), ({B} -> {C}), ({C} -> {D}) }";
+        String msg3FN = "Atributos: {A, B, C, D} Dependencias Funcionales: { ({B} -> {C}), ({A, B} -> {D}), ({C} -> {B}) }";
+        String msgBCFN = "Atributos: {A, B, C} Dependencias Funcionales: { ({A, B} -> {C}) }";
+        assertEquals(msg2FN, this.secondFormRelation.toString());
+        assertEquals(msg3FN, this.thirdFormRelation.toString());
+        assertEquals(msgBCFN, this.boyceCoddFormRelation.toString());
+    }
+    
+    /**
+     * Test method for {@link datastructures.Relation#toString()}.
+     */
+    @Test
+    public void testToStringTestNullAttrJoint() {
+        String msg = "Sin Atributos Dependencias Funcionales: { ({A, B} -> {C}) }";
+        this.nullRelation.setDFJoint(boyceCoddFormRelationDFJoint);
+        assertEquals(msg, this.nullRelation.toString());
+    }
+    
+    /**
+     * Test method for {@link datastructures.Relation#toString()}.
+     */
+    @Test
+    public void testToStringTestNullDFJoint() {
+        String msg = "Atributos: {A, B, C} Sin Dependencias Funcionales";
+        this.nullRelation.settAttrJoint(boyceCoddFormRelationAttributes);
+        assertEquals(msg, this.nullRelation.toString());
+    }
+    
+    /**
+     * Test method for {@link datastructures.Relation#toString()}.
+     */
+    @Test
+    public void testToStringTestVoidValuesRelation() {
+        String msg = "Sin Atributos Sin Dependencias Funcionales";
+        assertEquals(msg, this.nullRelation.toString());
+    }
+
+}
