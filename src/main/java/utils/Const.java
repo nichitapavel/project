80716eb3fb0e5c273218fc1ba3f6166201f977f0
Pavel <nichitapavel@live.com>
2016-04-03 17:11:13 +0100

    Add constants to Const class
    Add constants to Const class: ANTECEDENT with value "Antecedent" and
    CONSEQUEDENTwith value "Consequent".

--- src/main/java/utils/Const.java
+++ src/main/java/utils/Const.java
@@ -10,6 +10,8 @@ package utils;
 public final class Const {
     public static final String ATTRJOINT_NOT_INIT = "AttributeJoint not initialized";
     public static final String FD_NOT_INIT = "FunctionalDependency not initialized";
+    public static final String ANTECEDENT = "Antecedent";
+    public static final String CONSEQUEDENT = "Consequent";
     
     private Const() {
         // Private constructor to prevent instantiation.
